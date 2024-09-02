f4d9fcb8f5414eadcd86d73fe7cb157b24658068
Thanachai Bua <thanachai.bua@rmutto.ac.th>
2024-09-02 22:44:49 +0700

    final

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '1234',
-  database: 'computer_inventory'
+  database: 'ThanachaiDatabase'
 });
 
 db.connect(err => {
